83a2b2c4f4aff16a47ef47d2367c1a68eb5d41be
my9r <110807105+my9r@users.noreply.github.com>
2026-01-04 09:49:51 +0800

    upd

---
+++ tmp2.py
@@ -0,0 +1,28 @@
+import requests
+from multiprocessing import Process
+import time, os
+
+def test_website_pressure(url, requests_per_process=10):
+    for i in range(requests_per_process):
+        try:
+            response = requests.get(url)
+            print(f"Process {os.getpid()} - Request {i+1}: Status {response.status_code}")
+        except Exception as e:
+            print(f"Process {os.getpid()} - Request {i+1}: Error {e}")
+
+if __name__ == "__main__":
+    import os
+
+    # url = input("Enter the URL to test: ").strip()
+    url = "https://dingdingworld.com/images/portrait.png"
+    # num_processes = int(input("Enter number of processes: ").strip())
+    num_processes = 5000
+    # requests_per_process = int(input("Enter number of requests per process: ").strip())
+    requests_per_process = 1000
+    
+    processes = []
+    start_time = time.time()
+    for _ in range(num_processes):
+        p = Process(target=test_website_pressure, args=(url, requests_per_process),daemon=True)
+        processes.append(p)
+        p.start()
